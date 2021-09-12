a2dfd34be9945b1764e87a125c263fcc6a68565c
bzamora020 <bryan.aldair.zamora@gmail.com>
2021-09-12 15:10:42 -0700

    array3 init

---
+++ arrays/practiceArray3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <vector>
+
+    std::vector<int> sortedSquares(std::vector<int>& nums) {
+        std::vector<int> nums2 = nums;
+        
+
+    }
+
+int main()
+{
+
+    std::vector<int> test = {1123, 1222, 3330, 441};
+
+    sortedSquares(test);
+
+    return 0;
+}
