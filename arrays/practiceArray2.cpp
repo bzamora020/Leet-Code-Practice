ff4b717ee83b707b2cbe6d933e34635478456cd3
bzamora020 <bryan.aldair.zamora@gmail.com>
2021-09-12 14:57:08 -0700

    practice array 2 done

--- arrays/practiceArray2.cpp
+++ arrays/practiceArray2.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 
 #include <vector>
+#include <string>
 
 int findNumbers(std::vector<int> &nums)
 {
-    
+    int counter = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        std::string number = "";
+        number = std::to_string(nums[i]);
+        if ((number.size() % 2) == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
 }
 
 int main()
 {
+
+    std::vector<int> test = {1123, 1222, 3330, 441};
+
+    findNumbers(test);
 
     return 0;
 }
